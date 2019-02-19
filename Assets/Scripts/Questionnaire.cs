bc5d858e19146de3d4826bdbe2b05903c289432d
Bakhtiar <itsproinc@gmail.com>
2019-02-20 00:36:07 +0700

    Update

---
+++ Assets/Scripts/Questionnaire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questionnaire : MonoBehaviour
+{
+    // Questionnaire
+    /*  | - Data stribng split
+        Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4)
+
+        ; - Split question
+        Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4);
+        Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4)
+     */
+    public TextAsset [] w1_questionnaire; // World 1
+
+}
