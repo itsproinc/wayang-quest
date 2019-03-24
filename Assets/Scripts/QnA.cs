c5b6877c8df53d1271de25f841216b633ba4593f
Bakhtiar <itsproinc@gmail.com>
2019-03-24 13:31:04 +0700

    Game update

---
+++ Assets/Scripts/QnA.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QnA : MonoBehaviour
+{
+    Text questionDisplay;
+    List<Text> choicesDisplay = new List<Text> ();
+    QuestionInterpreter questionInterpreter;
+    GameObject [] choicesDisplayGameObject;
+    private void Start ()
+    {
+        questionDisplay = GameObject.FindGameObjectWithTag ("QuestionDisplay").GetComponent<Text> ();
+        questionInterpreter = GetComponent<QuestionInterpreter> ();
+
+        choicesDisplayGameObject = GameObject.FindGameObjectsWithTag ("ChoicesDisplay");
+
+        foreach (GameObject ChoicesDisplayGameObject in choicesDisplayGameObject)
+            choicesDisplay.Add (ChoicesDisplayGameObject.transform.GetChild (0).GetComponent<Text> ());
+
+        ShowQnA ();
+    }
+
+    [TextArea]
+    int [] availableQuestionData;
+    string rawQuestionData;
+    string [] questionData;
+    List<String> allChoices = new List<String> ();
+    int corretChoice;
+    private void ShowQnA ()
+    {
+        rawQuestionData = questionInterpreter.GetQuestionData (0, 0, 0);
+        questionData = rawQuestionData.Split (new char [] { '|' }, StringSplitOptions.None);
+
+        allChoices.Clear ();
+        for (int i = 0; i < (questionData.Length - 2); i++)
+        {
+            allChoices.Add (questionData [i + 1]);
+            availableChoices.Add (i);
+        }
+
+        StartCoroutine (ShowQuestion (questionData [0]));
+        corretChoice = int.Parse (questionData [questionData.Length - 1]);
+    }
+
+    // Typewritter effect for question
+    public float TypewritterSpeedDelay = 0.05f;
+    bool doneShowQuestion;
+    private IEnumerator ShowQuestion (String Question)
+    {
+        foreach (char QuestionLetters in Question)
+        {
+            questionDisplay.text = questionDisplay.text + QuestionLetters.ToString ();
+            yield return new WaitForSeconds (TypewritterSpeedDelay);
+        }
+
+        ShowChoices ();
+    }
+
+    List<int> availableChoices = new List<int> ();
+    int [] choicesPlacement;
+    private void ShowChoices ()
+    {
+        choicesPlacement = new int [allChoices.Count];
+        for (int i = 0; i < (questionData.Length - 2); i++)
+        {
+            // Pick random
+            int random = UnityEngine.Random.Range (0, availableChoices.Count);
+            choicesDisplay [i].text = allChoices [availableChoices [random]];
+            choicesPlacement [i] = availableChoices [random];
+            availableChoices.Remove (availableChoices [random]);
+        }
+    }
+}
--- Assets/Scripts/QuestionInterpreter.cs
+++ Assets/Scripts/QuestionInterpreter.cs
@@ -1,34 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿ using System.Collections.Generic;
+ using System.Collections;
+ using System;
+ using UnityEngine;
 
-public class QuestionInterpreter : MonoBehaviour
-{
-    // Questionnaire
-    /*  | - Data stribng split
-        Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4)
+ public class QuestionInterpreter : MonoBehaviour
+ {
+     // Questionnaire
+     /*  | - Data string split
+         Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4)
 
-        ; - Split question
-        Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4);
-        Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4)
-     */
+         ; - Split question
+         Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4);
+         Pertanyaan | Opsi 1 | Opsi 2 | Opsi 3 | Opsi 4 | Jawaban (0, 4)
+      */
 
-    public TextAsset [] w1_questionnaire; // World 1
-    TextAsset questionnaire;
-    private void LoadQuestion (int World, int Level)
-    {
-        switch (World)
-        {
-            case 0: // World 1
-                questionnaire = w1_questionnaire [Level];
-                break;
-        }
+     public TextAsset [] w1_questionnaire; // World 1
+     TextAsset questionnaire;
+     private void LoadQuestion (int World, int Level)
+     {
+         switch (World)
+         {
+             case 0: // World 0
+                 questionnaire = w1_questionnaire [Level];
+                 break;
+         }
 
-        InterpetQuestion (questionnaire);
-    }
+         InterpetQuestion (questionnaire);
+     }
 
-    private void InterpetQuestion (TextAsset Questionnaire)
-    {
+     [TextArea]
+     public string readQuestionnaire;
+     public string [] questionDataPool;
+     private void InterpetQuestion (TextAsset Questionnaire)
+     {
+         readQuestionnaire = Questionnaire.text;
 
-    }
-}
+         questionDataPool = readQuestionnaire.Split (new char [] { ';' }, StringSplitOptions.None);
+     }
+
+     protected internal String GetQuestionData (int World, int Level, int QuestionNumber)
+     {
+         LoadQuestion (World, Level);
+         return questionDataPool [QuestionNumber];
+     }
+ }
